1cf918ae315b648c23c90c157ee8cecf7720b211
Александр Грачев <agrachev@neoflex.ru>
2024-07-30 08:29:54 +0000

    HW 5/15 impl added

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM rust:1.79.0-buster
+
+ARG USERNAME=moroz
+ARG USER_UID=1000
+ARG USER_GID=$USER_UID
+
+# Create the user
+RUN groupadd --gid $USER_GID $USERNAME \
+    && useradd -s /bin/bash --uid $USER_UID --gid $USER_GID -m $USERNAME \
+    #
+    # [Optional] Add sudo support. Omit if you don't need to install software after connecting.
+    && apt-get update \
+    && apt-get install -y sudo \
+    && echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME \
+    && chmod 0440 /etc/sudoers.d/$USERNAME
+
+
+RUN rustup component add rustfmt
+RUN rustup component add clippy
+
+# ********************************************************
+# * Anything else you want to do like clean up goes here *
+# ********************************************************
+
+# [Optional] Set the default user. Omit if you want to keep the default as root.
+USER $USERNAME
--- src/lib.rs
+++ src/lib.rs
@@ -14,14 +14,34 @@ impl Item {
     }
 }
 
+pub trait CommonOps: Sized {
+    fn default_values() -> Self;
+
+    fn get_item(&self, item: Item) -> f64;
+
+    fn set_item(&mut self, item: Item, value: f64);
+
+    fn is_default(&self) -> bool {
+        let default: Self = Self::default_values();
+        self.get_item(Item::First) == default.get_item(Item::First)
+            && self.get_item(Item::Second) == default.get_item(Item::Second)
+            && self.get_item(Item::Third) == default.get_item(Item::Third)
+    }
+
+    fn sum(&self) -> f64 {
+        self.get_item(Item::First) + self.get_item(Item::Second) + self.get_item(Item::Third)
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Tuple(u32, f32, f64);
 
-impl Tuple {
-    pub fn default_values() -> Self {
+impl CommonOps for Tuple {
+    fn default_values() -> Self {
         Self(0, 0.0, 0.0)
     }
 
-    pub fn get_item(&self, item: Item) -> f64 {
+    fn get_item(&self, item: Item) -> f64 {
         match item {
             Item::First => self.0 as _,
             Item::Second => self.1 as _,
@@ -29,52 +49,73 @@ impl Tuple {
         }
     }
 
-    pub fn set_item(&mut self, item: Item, value: f64) {
+    fn set_item(&mut self, item: Item, value: f64) {
         match item {
             Item::First => self.0 = value as _,
             Item::Second => self.1 = value as _,
             Item::Third => self.2 = value,
         };
     }
-
-    pub fn is_default(&self) -> bool {
-        self.0 == 0 && self.1 == 0.0 && self.2 == 0.0
-    }
-
-    pub fn sum(&self) -> f64 {
-        self.0 as f64 + self.1 as f64 + self.2
-    }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Array([f64; 3]);
 
-impl Array {
-    pub fn default_values() -> Self {
+impl CommonOps for Array {
+    fn default_values() -> Self {
         Self([0.0; 3])
     }
 
-    pub fn get_item(&self, item: Item) -> f64 {
+    fn get_item(&self, item: Item) -> f64 {
         self.0[item.index()]
     }
 
-    pub fn set_item(&mut self, item: Item, value: f64) {
+    fn set_item(&mut self, item: Item, value: f64) {
         self.0[item.index()] = value
     }
+}
 
-    pub fn is_default(&self) -> bool {
-        for value in &self.0 {
-            if *value != 0.0 {
-                return false;
-            }
-        }
-        true
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    fn sum_should_work_correctly(mut v: impl CommonOps) {
+        v.set_item(Item::First, 1.0);
+        v.set_item(Item::Second, 2.0);
+        v.set_item(Item::Third, 3.0);
+        assert!(v.sum() == 6.0)
     }
 
-    pub fn sum(&self) -> f64 {
-        let mut sum = 0.0;
-        for value in &self.0 {
-            sum += *value;
+    fn is_default_should_work_correctly<T: CommonOps + PartialEq>(v: T) {
+        let d: T = T::default_values();
+        assert!(d.is_default());
+        if v == d {
+            assert!(v.is_default())
+        } else {
+            assert!(!v.is_default())
         }
-        sum
+    }
+
+    #[test]
+    pub fn sum_should_work_correctly_for_tuple() {
+        sum_should_work_correctly(Tuple::default_values())
+    }
+
+    #[test]
+    pub fn sum_should_work_correctly_for_array() {
+        sum_should_work_correctly(Array::default_values())
+    }
+
+    #[test]
+    pub fn is_default_should_work_correctly_for_tuple() {
+        is_default_should_work_correctly(Tuple::default_values());
+        is_default_should_work_correctly(Tuple(1, 2_f32, 3_f64));
+    }
+
+    #[test]
+    pub fn is_default_should_work_correctly_for_array() {
+        is_default_should_work_correctly(Array::default_values());
+        is_default_should_work_correctly(Array([1_f64, 2_f64, 3.0]));
     }
 }
